304a945f7cba280ed9050177e980e66bdd406911
Patrick Yee <patrick.yee@gmail.com>
2016-09-07 23:13:23 -0700

    use older version of ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/profile
 apt-get update
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-apt-get install -y tomcat8 maven mysql-server
+apt-get install -y tomcat7 maven mysql-server
 
 # compile
 cd /vagrant
