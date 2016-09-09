d4012351e696fa2ebae38375718d5fbb7203f29e
Patrick Yee <patrick.yee@gmail.com>
2016-09-09 00:06:47 -0700

    fix compile issue

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
+# install packages
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-apt-get install -y tomcat7 maven mysql-server
+apt-get install -y openjdk-7-jdk tomcat7 maven mysql-server
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 echo "export JAVA_HOME=$JAVA_HOME" >> /etc/profile
 
 # compile
 cd /vagrant
-sudo -u vagrant mvn pre-clean && mvn clean package
+sudo -u vagrant mvn pre-clean
+sudo -u vagrant mvn compile
+sudo -u vagrant mvn package
 
 # setup database
 MYSQL_PWD=root mysql -u root -e "create database as2 collate=latin1_general_cs"
