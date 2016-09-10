1cd198a90efdb605187ba316078b5efa8a34f5d3
Patrick Yee <patrick.yee@gmail.com>
2016-09-10 16:55:30 -0700

    database creation  only if not exists. install mysql jdbc driver, fix tomcat admin permission and fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt-get update
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password root" | debconf-set-selections
-apt-get install -y openjdk-7-jdk tomcat7 maven mysql-server
+apt-get install -y openjdk-7-jdk tomcat7 maven mysql-server libmysql-java
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 echo "export JAVA_HOME=$JAVA_HOME" >> /etc/profile
 
@@ -16,18 +16,27 @@ sudo -u vagrant mvn compile
 sudo -u vagrant mvn package
 
 # setup database
-MYSQL_PWD=root mysql -u root -e "create database as2 collate=latin1_general_cs"
+MYSQL_PWD=root mysql -u root -e "create database if not exists as2 collate=latin1_general_cs"
 MYSQL_PWD=root mysql -u root -e "grant all on as2.* to 'corvus'@'localhost' identified by 'corvus'"
-MYSQL_PWD=root mysql -u root -e "create database ebms collate=latin1_general_cs"
+MYSQL_PWD=root mysql -u root -e "create database if not exists ebms collate=latin1_general_cs"
 MYSQL_PWD=root mysql -u root -e "grant all on ebms.* to 'corvus'@'localhost' identified by 'corvus'"
 MYSQL_PWD=corvus mysql -u corvus as2 < /vagrant/h2o-installer/sql/mysql_as2.sql
 MYSQL_PWD=corvus mysql -u corvus ebms < /vagrant/h2o-installer/sql/mysql_ebms.sql
 
 # deploy
+mv /etc/tomcat7/tomcat-users.xml /etc/tomcat7/tomcat-users.xml.bak
+cat <<EOF > /etc/tomcat7/tomcat-users.xml
+<?xml version='1.0' encoding='utf-8'?>
+<tomcat-users>
+  <role rolename="tomcat"/>
+  <role rolename="admin"/>
+  <user username="corvus" password="corvus" roles="tomcat,admin"/>
+</tomcat-users>
+EOF
 mkdir -p /home/vagrant/hermes_home/logs
 mkdir -p /tmp/h
 cd /tmp/h
-unzip /vagrant/target/hermes_bin.zip
+unzip /vagrant/target/hermes2_bin.zip
 rm -rf /tmp/h/plugins/corvus-sfrm
 find . -name *.xml -exec sed -i 's/@h2\.home@/\/home\/vagrant\/hermes_home/g' {} \;
 find . -name *.xml -exec sed -i 's/@as2PageletAdaptor@/hk\.hku\.cecid\.edi\.as2\.admin\.listener\.MessageHistoryPageletAdaptor/g' {} \;
