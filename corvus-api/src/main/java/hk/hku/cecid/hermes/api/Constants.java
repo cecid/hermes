3a9032d9e2ff71fb9139f25ccd2b94f179667600
Patrick Yee <patrick.yee@gmail.com>
2016-11-17 22:14:18 -0800

    add constant and error code class

---
+++ corvus-api/src/main/java/hk/hku/cecid/hermes/api/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2016 Center for E-Commerce Infrastructure Development, The
+ * University of Hong Kong (HKU). All Rights Reserved.
+ *
+ * This software is licensed under the GNU GENERAL PUBLIC LICENSE Version 2.0 [1]
+ *
+ * [1] http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+package hk.hku.cecid.hermes.api;
+
+
+public class Constants {
+    public static final String HEALTHY = "healthy";
+    public static final String CONTENT_TYPE = "application/vnd.api+json";
+    public static final String METHOD_GET = "GET";
+    public static final String METHOD_POST = "POST";
+    public static final String EBMS_PROTOCOL = "ebms";
+    public static final int DEFAULT_PARTNERSHIP_RETRY_INTERVAL = 60000;
+    public static final int DEFAULT_PARTNERSHIP_RETRY_COUNT = 3;
+}
---
+++ corvus-api/src/main/java/hk/hku/cecid/hermes/api/ErrorCode.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright(c) 2016 Center for E-Commerce Infrastructure Development, The
+ * University of Hong Kong (HKU). All Rights Reserved.
+ *
+ * This software is licensed under the GNU GENERAL PUBLIC LICENSE Version 2.0 [1]
+ *
+ * [1] http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ */
+
+package hk.hku.cecid.hermes.api;
+
+
+public class ErrorCode {
+    public static final int ERROR_UNKNOWN = 10000;
+    public static final int ERROR_MISSING_REQUIRED_PARAMETER = 10001;
+    public static final int ERROR_PROTOCOL_UNSUPPORTED = 10002;
+    public static final int ERROR_READING_DATABASE = 10003;
+    public static final int ERROR_WRITING_DATABASE = 10004;
+    public static final int ERROR_READING_REQUEST = 10005;
+    public static final int ERROR_PARSING_REQUEST = 10006;
+    public static final int ERROR_RECORD_ALREADY_EXIST = 10007;
+}
