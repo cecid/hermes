53c351649474fa5b7e3a4c64e20d91b47b22e608
Jeff Lao <jefflao@cecid.org>
2016-12-02 16:29:37 +0800

    Fix sql definitions

--- h2o-installer/sql/hsqldb_as2plus.sql
+++ h2o-installer/sql/hsqldb_as2plus.sql
@@ -1,59 +1,59 @@
 CREATE TABLE partnership (
-	partnership_id varchar NOT NULL,
-	subject varchar,
-	recipient_address varchar,
-	receipt_address varchar,
-	is_sync_reply varchar,
-	is_receipt_requested varchar,
-	is_outbound_sign_required varchar,
-	is_outbound_encrypt_required varchar,
-	is_outbound_compress_required varchar,
-	is_receipt_sign_required varchar,
-	is_inbound_sign_required varchar,
-	is_inbound_encrypt_required varchar,
-	sign_algorithm varchar,
-	encrypt_algorithm varchar,
-	mic_algorithm varchar,
-	as2_from varchar NOT NULL,
-	as2_to varchar NOT NULL,
+	partnership_id varchar(200) NOT NULL,
+	subject varchar(200),
+	recipient_address varchar(200),
+	receipt_address varchar(200),
+	is_sync_reply varchar(200),
+	is_receipt_requested varchar(200),
+	is_outbound_sign_required varchar(200),
+	is_outbound_encrypt_required varchar(200),
+	is_outbound_compress_required varchar(200),
+	is_receipt_sign_required varchar(200),
+	is_inbound_sign_required varchar(200),
+	is_inbound_encrypt_required varchar(200),
+	sign_algorithm varchar(200),
+	encrypt_algorithm varchar(200),
+	mic_algorithm varchar(200),
+	as2_from varchar(200) NOT NULL,
+	as2_to varchar(200) NOT NULL,
 	encrypt_cert binary,
 	verify_cert binary,
 	retries int,
 	retry_interval int,
-	is_disabled varchar NOT NULL,
-	is_hostname_verified varchar,
+	is_disabled varchar(200) NOT NULL,
+	is_hostname_verified varchar(200),
 	PRIMARY KEY (partnership_id)
 );
 
 CREATE TABLE message (
-	message_id varchar NOT NULL,
-	message_box varchar NOT NULL,
-	as2_from varchar NOT NULL,
-	as2_to varchar NOT NULL,
-	is_receipt varchar,
-	is_acknowledged varchar,
-	is_receipt_requested varchar,
-	receipt_url varchar,
-	mic_value varchar,
-	original_message_id varchar, -- message_id of the message that the receipt replies to 
-	primal_message_id varchar,-- The message_id of message which triggered "Resend as New Message"
-	has_resend_as_new varchar,
-	partnership_id varchar,
+	message_id varchar(200) NOT NULL,
+	message_box varchar(200) NOT NULL,
+	as2_from varchar(200) NOT NULL,
+	as2_to varchar(200) NOT NULL,
+	is_receipt varchar(200),
+	is_acknowledged varchar(200),
+	is_receipt_requested varchar(200),
+	receipt_url varchar(200),
+	mic_value varchar(200),
+	original_message_id varchar(200), -- message_id of the message that the receipt replies to 
+	primal_message_id varchar(200),-- The message_id of message which triggered "Resend as New Message"
+	has_resend_as_new varchar(200),
+	partnership_id varchar(200),
 	time_stamp timestamp NOT NULL,
-	status varchar NOT NULL,
-	status_desc varchar,
+	status varchar(200) NOT NULL,
+	status_desc varchar(200),
 	PRIMARY KEY (message_id, message_box)
 );
 
 CREATE TABLE repository (
-	message_id varchar NOT NULL,
-	message_box varchar NOT NULL,
+	message_id varchar(200) NOT NULL,
+	message_box varchar(200) NOT NULL,
 	content binary NOT NULL,
 	PRIMARY KEY (message_id, message_box)
 );
 
 CREATE TABLE raw_repository (
-	message_id varchar NOT NULL,
+	message_id varchar(200) NOT NULL,
 	content binary NOT NULL,
 	PRIMARY KEY (message_id)
 );
--- h2o-installer/sql/hsqldb_ebms.sql
+++ h2o-installer/sql/hsqldb_ebms.sql
@@ -1,80 +1,80 @@
 CREATE TABLE message (
-	message_id varchar,
-	message_box varchar,
-	message_type varchar,
-	from_party_id varchar,
-	from_party_role varchar,
-	to_party_id varchar,
-	to_party_role varchar,
-	cpa_id varchar,
-	service varchar,
-	action varchar,
-	conv_id varchar,
-	ref_to_message_id varchar,-- message_id of the message that the response replies to 
-	primal_message_id varchar,-- message_id of message which triggered "Resend as New Message"
-	has_resend_as_new varchar,
-	partnership_id varchar,
-	sync_reply varchar,
-	dup_elimination varchar,
-	ack_requested varchar,
-	ack_sign_requested varchar,
+	message_id varchar(200),
+	message_box varchar(200),
+	message_type varchar(200),
+	from_party_id varchar(200),
+	from_party_role varchar(200),
+	to_party_id varchar(200),
+	to_party_role varchar(200),
+	cpa_id varchar(200),
+	service varchar(200),
+	action varchar(200),
+	conv_id varchar(200),
+	ref_to_message_id varchar(200),-- message_id of the message that the response replies to 
+	primal_message_id varchar(200),-- message_id of message which triggered "Resend as New Message"
+	has_resend_as_new varchar(200),
+	partnership_id varchar(200),
+	sync_reply varchar(200),
+	dup_elimination varchar(200),
+	ack_requested varchar(200),
+	ack_sign_requested varchar(200),
 	sequence_no integer,
 	sequence_status integer,
 	sequence_group integer,
 	time_to_live timestamp,
 	time_stamp timestamp,
 	timeout_time_stamp timestamp,
-	status varchar,
-	status_description varchar,
+	status varchar(200),
+	status_description varchar(200),
 	PRIMARY KEY (message_id, message_box)
 );
 
 CREATE TABLE repository (
-	message_id varchar,
-	content_type varchar,
+	message_id varchar(200),
+	content_type varchar(200),
 	content binary,
 	time_stamp timestamp,
-	message_box varchar,
+	message_box varchar(200),
 	PRIMARY KEY (message_id, message_box)
 );
 
 CREATE TABLE outbox (
-	message_id varchar,
+	message_id varchar(200),
 	retried integer,
 	PRIMARY KEY (message_id)
 );
 
 CREATE TABLE inbox (
-	message_id varchar,
+	message_id varchar(200),
 	order_no bigint,
 	PRIMARY KEY (message_id)
 );
 
 CREATE TABLE partnership (
-	partnership_id varchar,
-	cpa_id varchar,
-	service varchar,
-	action varchar,
-	transport_protocol varchar,
-	transport_endpoint varchar,
-	is_hostname_verified varchar,
-	sync_reply_mode varchar,
-	ack_requested varchar,
-	ack_sign_requested varchar,
-	dup_elimination varchar,
-	actor varchar,
-	disabled varchar,
+	partnership_id varchar(200),
+	cpa_id varchar(200),
+	service varchar(200),
+	action varchar(200),
+	transport_protocol varchar(200),
+	transport_endpoint varchar(200),
+	is_hostname_verified varchar(200),
+	sync_reply_mode varchar(200),
+	ack_requested varchar(200),
+	ack_sign_requested varchar(200),
+	dup_elimination varchar(200),
+	actor varchar(200),
+	disabled varchar(200),
 	retries integer,
 	retry_interval integer,
-	persist_duration varchar,
-	message_order varchar,
-	sign_requested varchar,
+	persist_duration varchar(200),
+	message_order varchar(200),
+	sign_requested varchar(200),
 	sign_cert binary,
-	ds_algorithm varchar,
-	md_algorithm varchar,
-	encrypt_requested varchar,
+	ds_algorithm varchar(200),
+	md_algorithm varchar(200),
+	encrypt_requested varchar(200),
 	encrypt_cert binary,
-	encrypt_algorithm varchar,
+	encrypt_algorithm varchar(200),
 	PRIMARY KEY (partnership_id)
 );
 
